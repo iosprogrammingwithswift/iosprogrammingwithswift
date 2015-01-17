8a058b7bbf538e3ae31604aef0916bef46e03319
Florian Bachmann <flori@timtomtam.de>
2015-01-17 12:55:35 +0100

    swift 97 playground updated

---  Swift_07_WeatherExercise.playground/section-1.swift
+++  Swift_07_WeatherExercise.playground/section-1.swift
@@ -2,6 +2,25 @@
 
 import UIKit
 
-var loc = Location(ID: 121739, date: 1421487086, name: "Würzburg", lat: 49.79, lon: 9.94, message: "Fehler", country: "Germany", sunrise: 1421478603, sunset: 1421509845)
+var a = NSDate(timeIntervalSince1970: NSTimeInterval(1421509845))
+println(a)
 
-var wether = Weather(temp: 275.65, humidity: 94, temp_min: 273.15, temp_max: 277.25, pressure: 1019.162, weatherIcon: "rain")
+enum WeatherIcon:String {
+    
+    case Rain = "Rainy"
+    case Cloud = "Cloudy"
+    case Sun = "Sunny"
+    case SunnyWithClouds = "SunnyWithClouds"
+    var image: UIImage{
+        return UIImage(named: self.rawValue)!
+    }
+    
+}
+
+
+var loc = Location(ID: 121739, name: "Würzburg", lat: 49.79, lon: 9.94, message: "Fehler", country: "Germany", sunrise: 1421478603, sunset: 1421509845)
+
+var weather = Weather(dt: 1421509845,temp: 275.65, humidity: 94, temp_min: 273.15, temp_max: 277.25, pressure: 1019.162, weatherIcon: "rain", sea_level:0, grnd_level:0)
+
+
+
