dda44b2a720056be7160f028e2261145abd1faf6
Florian Bachmann <flori@timtomtam.de>
2015-01-17 12:05:17 +0100

    weather exercise

---
+++  Swift_07_WeatherExercise.playground/section-1.swift
@@ -0,0 +1,7 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var loc = Location(ID: 121739, date: 1421487086, name: "Würzburg", lat: 49.79, lon: 9.94, message: "Fehler", country: "Germany", sunrise: 1421478603, sunset: 1421509845)
+
+var wether = Weather(temp: 275.65, humidity: 94, temp_min: 273.15, temp_max: 277.25, pressure: 1019.162, weatherIcon: "rain")
