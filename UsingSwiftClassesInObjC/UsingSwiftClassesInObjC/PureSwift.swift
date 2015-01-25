ae9947141b5c911325d2c27993b8073363fa94b0
Florian Bachmann <flori@timtomtam.de>
2015-01-25 22:51:16 +0100

    added demo projects

---
+++ UsingSwiftClassesInObjC/UsingSwiftClassesInObjC/PureSwift.swift
@@ -0,0 +1,17 @@
+//
+//  PureSwift.swift
+//  UsingSwiftClassesInObjC
+//
+//  Created by Flori on 22.01.15.
+//  Copyright (c) 2015 Flori & Andy. All rights reserved.
+//
+
+import UIKit
+
+@objc class PureSwift: NSObject {
+    var someProperty:Int = 5
+    
+    func myFunc() {
+        println("hello")
+    }
+}
---
+++ UsingSwiftClassesInObjC/UsingSwiftClassesInObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UsingSwiftClassesInObjC
+//
+//  Created by Flori on 22.01.15.
+//  Copyright (c) 2015 Flori & Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
