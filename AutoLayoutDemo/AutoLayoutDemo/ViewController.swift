215081ca0676d05d465703f4d1814cb5389ce21b
Florian Bachmann <bachmann.florian@gmail.com>
2015-12-07 12:44:27 +0100

    example projects

---
+++ AutoLayoutDemo/AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Flori on 05.12.15.
+//  Copyright © 2015 Flori & Andy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
